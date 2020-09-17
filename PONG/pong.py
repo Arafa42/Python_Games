09eecd19ebd303c6ba9d0d1217e966f8ff1d7f23
Arafa Yoncalik <55528241+Arafa42@users.noreply.github.com>
2020-09-18 01:01:32 +0200

    score added

--- PONG/pong.py
+++ PONG/pong.py
@@ -1,5 +1,6 @@
 import turtle
 import os
+import time
 
 #WINDOW INIT
 window = turtle.Screen()
@@ -7,6 +8,13 @@ window.title("PONG")
 window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)
+
+#SCORE
+score_a = 0
+score_b = 0
+
+#GAME OVER RESET
+reset = False
 
 #PADDLE 1
 paddle1 = turtle.Turtle()
@@ -33,8 +41,20 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(-0,0)
-ball.dx = 0.4
-ball.dy = -0.4
+ball.dx = 0.2
+ball.dy = -0.2
+
+#GAME OVER TURTLE
+game_over = turtle.Turtle()
+
+#SCORETEXT
+score = turtle.Turtle()
+score.speed(0)
+score.color("white")
+score.penup()
+score.hideturtle()
+score.goto(0,260)
+score.write("PLAYER A : 0       PLAYER B : 0", align="center", font=("Courier", 24, "normal"))
 
 #MOVEMENT
 def paddle1_up():
@@ -57,12 +77,14 @@ def paddle2_down():
     y-=20
     paddle2.sety(y)
 
+
 #KEYBOARD
 window.listen()
 window.onkeypress(paddle1_up, "z")
 window.onkeypress(paddle1_down, "s")
 window.onkeypress(paddle2_up, "Up")
 window.onkeypress(paddle2_down, "Down")
+
 
 #GAME LOOP
 while True:
@@ -84,17 +106,46 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0,0)
         ball.dx *= -0.6
+        score_a += 1
+        score.clear()
+        score.write("PLAYER A : {}       PLAYER B : {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
+
 
     if ball.xcor() < -390:
         ball.goto(0,0)
         ball.dx *= -0.6
+        score_b += 1
+        score.clear()
+        score.write("PLAYER A : {}       PLAYER B : {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
 
-#PADDLE & BALL COLLISION 
+
+    #PADDLE & BALL COLLISION 
     if ball.xcor() > 340 and (ball.ycor() < paddle2.ycor() + 40 and ball.ycor() > paddle2.ycor() -40):
         ball.setx(340)
-        ball.dx *= -1
-        collision = True    
+        ball.dx *= -1    
     
     if ball.xcor() < -340 and (ball.ycor() < paddle1.ycor() + 40 and ball.ycor() > paddle1.ycor() -40):
         ball.setx(-340)
         ball.dx *= -1
+
+    # #GAME OVER
+    # if (score_a or score_b) == 3:
+
+    #     paddle1.hideturtle()
+    #     paddle2.hideturtle()
+    #     ball.hideturtle()
+    #     score.hideturtle()
+    #     game_over.speed(0)
+    #     game_over.color("red")
+    #     game_over.penup()
+    #     game_over.goto(0,0)
+    #     game_over.hideturtle()
+    #     game_over.pendown()
+    #     game_over.write("GAME OVER : RESTART IN 3 SECONDS", align="center", font=("Courier", 24, "normal"))
+    #     time.sleep(3)
+    #     game_over.clear()
+    #     score_a = 0
+    #     score_b = 0
+       
+        
+
