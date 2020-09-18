4757c4a44c913e2bba886126c5d2a4629bdaa1b0
Arafa Yoncalik <55528241+Arafa42@users.noreply.github.com>
2020-09-18 16:48:47 +0200

    selection added

--- CONNECT4/connect4.py
+++ CONNECT4/connect4.py
@@ -4,5 +4,15 @@ def create_board():
     board =  numpy.zeros((6,7))
     return board
 
+board = create_board()
+game_over = False
+turn = 0
 
-print(create_board())
+while not game_over:
+    if turn == 0:
+        selection = int(input("Player 1, choose between (0-6)"))
+    else:
+        selection = int(input("Player 2, choose between (0-6)"))
+
+    turn+=1
+    turn = turn %2
