74e9a530ab95fdb00b1d4c1662b1f692482c4a9d
Arafa Yoncalik <55528241+Arafa42@users.noreply.github.com>
2020-09-20 23:38:58 +0200

    cleanup

--- CONNECT4/connect4.py
+++ CONNECT4/connect4.py
@@ -3,19 +3,15 @@ import numpy
 ROW = 6
 COL = 7
 
-
 def create_board():
     board = numpy.zeros((ROW, COL))
     return board
 
-
 def drop_piece(board, row, col, piece):
     board[row][col] = piece
 
-
 def is_valid_loc(board, col):
     return board[5][col] == 0
-
 
 def next_available_row(board, col):
     for r in range(ROW):
@@ -45,11 +41,6 @@ def win_check(board,piece):
             if board[r][c] == piece and board[r-11][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
                 return True
 
-
-
-
-
-
 board = create_board()
 print(board)
 game_over = False
@@ -66,7 +57,6 @@ while not game_over:
                 print("PLAYER 1 WINS !")
                 game_over = True
 
-
     else:
         colInput = int(input("Player 2, choose between (0-6)"))
         if(is_valid_loc(board,colInput)):
@@ -78,7 +68,5 @@ while not game_over:
                 game_over = True
 
     reverse_print_board()
-
-
     turn += 1
     turn = turn % 2
